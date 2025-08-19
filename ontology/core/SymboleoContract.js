4fdc7e6addf9b52d2fcda71c3973b63d9f7c2e97
Sofana Alfuhaid <sfuhaid@yahoo.com>
2025-08-19 11:57:31 -0400

    Update core library

--- ontology/core/SymboleoContract.js
+++ ontology/core/SymboleoContract.js
@@ -794,18 +794,13 @@ findRole(aRole){
   //check attributes of key/certificate for each role  
   authenticate(inRole, inName, inOrg, inDept,aContract ) {
       const objRole = this.findObject(inName, inRole, aContract)
-      console.log("objRole from Core")
-      console.log(objRole)
       if(objRole != null){
         if (inOrg === objRole.org._value && inDept === objRole.dept._value){   
           return objRole
         }else{
-          console.log("first null")
-          console.log(objRole)
           return null
          }
         }else{
-          console.log("second null")
           return null
         }
       }
