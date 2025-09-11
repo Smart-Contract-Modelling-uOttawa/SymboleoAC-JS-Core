060d06a4f65f3d777c5d8942b7665d9c0d96c220
Sofana Alfuhaid <sfuhaid@yahoo.com>
2025-09-11 13:56:42 -0400

    Update core library

--- ontology/core/SymboleoContract.js
+++ ontology/core/SymboleoContract.js
@@ -2,6 +2,8 @@ const { Event } = require('./Event.js');
 const { Party } = require('./Party.js');
 //sofana-AC
 const { Resource } = require('./Resource.js');
+const { ACPolicy } = require('./ACPolicy.js');
+const { Rule } = require('./Rule.js');
 
 
 const { Events } = require('../../Events.js');
